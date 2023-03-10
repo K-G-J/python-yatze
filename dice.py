dc457d6086186a038119c69ae71420943ae9f515
Kate Johnson <johnson.kat.g@gmail.com>
2023-03-10 14:38:01 -0500

    added Hand and YatzyHand classes

--- dice.py
+++ dice.py
@@ -8,33 +8,37 @@ class Die:
         if not isinstance(sides, int) or not isinstance(value, int):
             raise ValueError('Sides and value must be whole numbers')
         self.value = value or random.randint(1, sides)
-        
+
     def __int__(self):
         return self.value
-    
+
     def __eq__(self, other):
         return int(self) == other
-    
+
     def __ne__(self, other):
         return int(self) != other
-    
+
     def __gt__(self, other):
         return int(self) > other
-    
+
     def __lt__(self, other):
         return int(self) < other
-    
+
     def __ge__(self, other):
         return int(self) >= other
-    
+
     def __le__(self, other):
         return int(self) <= other
-    
+
     def __add__(self, other):
         return int(self) + other
-    
+
     def __radd__(self, other):
         return int(self) + other
+
+    def __repr__(self):
+        return str(self.value)
+
 
 class D6(Die):
     def __init__(self, value=0):
